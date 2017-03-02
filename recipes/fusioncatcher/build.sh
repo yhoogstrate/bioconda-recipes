7deb72b127caf230abc0d27a27b82fe165d32f43
yhoogstrate <y.hoogstrate@gmail.com>
2017-03-02 14:56:11 +0100

    FusionCatcher: changes to get conda-build started

--- recipes/fusioncatcher/build.sh
+++ recipes/fusioncatcher/build.sh
@@ -1,17 +1,21 @@
 #!/bin/bash
 
-mkdir -p "$PREFIX/bin"
-mkdir -p "$PREFIX/etc"
-mkdir -p "$PREFIX/doc"
-mkdir -p "$PREFIX/data"
-mkdir -p "$PREFIX/test"
+mkdir -p $PREFIX/bin
+mkdir -p $PREFIX/etc
+mkdir -p $PREFIX/doc
+mkdir -p $PREFIX/data
+mkdir -p $PREFIX/test
 
-cp -r * "$PREFIX/"
-chmod +x "$PREFIX/bin/*.py"
-chmod +x "$PREFIX/bin/*.sh"
 
-sed -i "s/\/apps\/fusioncatcher/$PREFIX/g" "$PREFIX/etc/configuration.cfg"
+mv $(find . | grep configuration.cfg) $PREFIX/etc
+mv bin/* "$PREFIX/bin"
 
+ls -als $PREFIX/bin/*.py
+
+chmod +x $PREFIX/bin/*.py
+chmod +x $PREFIX/bin/*.sh
+
+sed -i.bak 's#/apps/fusioncatcher#'$PREFIX'#g' $PREFIX/etc/configuration.cfg
 
 #"$PYTHON" "$PREFIX/bin/update-config.py" -w
 
